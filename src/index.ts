2acfbdd0bebb3e5742ad20f3eee135b03c6f560e
Riccardo Civardi <riccardo.civardi@net4market.com>
2024-02-05 15:09:54 +0100

    fix

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,5 @@ import TestComponent from "@/components/test/test-component";
 import MyElement from "@/components/test2/my-element";
 import BbToDoList from "@/components/todoList/bb-todoList";
 import BbMyPanel from "./components/bbMyPanel/bb-my-panel";
-import SuperTailwindElement from "./superTailwindElement/super-tailwind-element";
 
-export {
-  TestComponent,
-  MyElement,
-  BbToDoList,
-  BbMyPanel,
-  SuperTailwindElement,
-};
+export { TestComponent, MyElement, BbToDoList, BbMyPanel };
